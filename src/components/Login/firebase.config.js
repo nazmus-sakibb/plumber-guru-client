0940fd439685e0d1a9ab0320a1666de4ab2b1242
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-04-18 19:54:07 +0600

    firebase deployed

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyA6OgHYymZjycWMCaFZD5dJh757ej0REpE",
-    authDomain: "nexus-valley.firebaseapp.com",
-    projectId: "nexus-valley",
-    storageBucket: "nexus-valley.appspot.com",
-    messagingSenderId: "435548676632",
-    appId: "1:435548676632:web:cac68a248e0846794e596d"
-  };
+  apiKey: "AIzaSyBF6ZuSFfFuxgO_wJn5aBa15UIRFQsZL1I",
+  authDomain: "plumber-guru.firebaseapp.com",
+  projectId: "plumber-guru",
+  storageBucket: "plumber-guru.appspot.com",
+  messagingSenderId: "743602588057",
+  appId: "1:743602588057:web:a0fc4fbb2990e4e96c2b08"
+};
 
-  export default firebaseConfig;
+export default firebaseConfig;
